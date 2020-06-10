efdb16063cb2393286e67c16fd69813509729d42
Smith <fengfei153@aliyun.com>
2020-06-10 18:45:24 +0800

    u

--- SJUtilsPlugin.js
+++ SJUtilsPlugin.js
@@ -1,43 +1,43 @@
 var exec = require('cordova/exec');
-function SJUtilsPlugin(){};
+function SJUtilsPlugin() { };
 
-SJUtilsPlugin.prototype.getVersionCode = function(success){
-	exec(success, null, "SJUtilsPlugin", "getVersionCode", []);
+SJUtilsPlugin.prototype.getVersionCode = function (success) {
+    exec(success, null, "SJUtilsPlugin", "getVersionCode", []);
 };
-SJUtilsPlugin.prototype.update = function(updateUrl, success){
-	exec(success, null, "SJUtilsPlugin", "update", [updateUrl]);
+SJUtilsPlugin.prototype.update = function (updateUrl, success) {
+    exec(success, null, "SJUtilsPlugin", "update", [updateUrl]);
 };
-SJUtilsPlugin.prototype.getWifiSSID = function(success){
-	exec(success, null, "SJUtilsPlugin", "getWifiSSID", []);
+SJUtilsPlugin.prototype.getWifiSSID = function (success) {
+    exec(success, null, "SJUtilsPlugin", "getWifiSSID", []);
 };
-SJUtilsPlugin.prototype.smartConfig = function(ssid, bssid, password, tastCount, success, error){
-	setTimeout(function(){
-		exec(success, error, "SJUtilsPlugin", "smartConfig", [ssid, bssid, password, tastCount]);
-	}, 30);
+SJUtilsPlugin.prototype.smartConfig = function (ssid, bssid, password, tastCount, success, error) {
+    setTimeout(function () {
+        exec(success, error, "SJUtilsPlugin", "smartConfig", [ssid, bssid, password, tastCount]);
+    }, 30);
 };
-SJUtilsPlugin.prototype.cancelSmartConfig = function(success){
-	exec(success, null, "SJUtilsPlugin", "cancelSmartConfig", []);
+SJUtilsPlugin.prototype.cancelSmartConfig = function (success) {
+    exec(success, null, "SJUtilsPlugin", "cancelSmartConfig", []);
 };
-SJUtilsPlugin.prototype.sendBroadCast = function(sendIP, sendMess, success){
-	exec(success, null, "SJUtilsPlugin", "sendBroadCast", [sendIP, sendMess]);
+SJUtilsPlugin.prototype.sendBroadCast = function (sendIP, sendMess, success) {
+    exec(success, null, "SJUtilsPlugin", "sendBroadCast", [sendIP, sendMess]);
 };
-SJUtilsPlugin.prototype.receiveBroadCast = function(success){
-	exec(success, null, "SJUtilsPlugin", "receiveBroadCast", []);
+SJUtilsPlugin.prototype.receiveBroadCast = function (success) {
+    exec(success, null, "SJUtilsPlugin", "receiveBroadCast", []);
 };
-SJUtilsPlugin.prototype.getPreferredLanguage = function(success){
-	exec(success, null, "SJUtilsPlugin", "getPreferredLanguage", []);
+SJUtilsPlugin.prototype.getPreferredLanguage = function (success) {
+    exec(success, null, "SJUtilsPlugin", "getPreferredLanguage", []);
 };
-SJUtilsPlugin.prototype.showMsg = function(msg){
-	exec(null, null, "SJUtilsPlugin", "showMsg", [msg]);
+SJUtilsPlugin.prototype.showMsg = function (msg) {
+    exec(null, null, "SJUtilsPlugin", "showMsg", [msg]);
 };
-SJUtilsPlugin.prototype.hideMsg = function(){
-	exec(null, null, "SJUtilsPlugin", "hideMsg", []);
+SJUtilsPlugin.prototype.hideMsg = function () {
+    exec(null, null, "SJUtilsPlugin", "hideMsg", []);
 };
-SJUtilsPlugin.prototype.connectWiFi = function(ssid, password, success, error){
-	success(0);
+SJUtilsPlugin.prototype.connectWiFi = function (ssid, password, success, error) {
+    success(0);
 };
 
-if(!window.plugins) {
-	window.plugins = {};
+if (!window.plugins) {
+    window.plugins = {};
 }
 module.exports = new SJUtilsPlugin();
